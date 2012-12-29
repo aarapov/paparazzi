ab5c9e10a80017448d8f8029278cdbf4241666db
Artem Arapov <artem.arapov@gmail.com>
2012-12-29 22:02:21 +0200

    Added new modules

---
+++ domains/src/main/java/com/paparazzi/App.java
@@ -0,0 +1,13 @@
+package com.paparazzi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
